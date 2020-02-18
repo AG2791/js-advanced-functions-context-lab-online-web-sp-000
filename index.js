52f55720123a551f54b6db7d52b658e96f13737d
AJ Guiles <ashiguiles@gmail.com>
2020-02-18 23:11:24 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -84,14 +84,16 @@ let wagesEarnedOnDate = function(workDate){
     return parseFloat(rawWage.toString())
 }
 
+let findEmployeebyFirstName = function(srcArray, firstName) {
+  return srcArray.find(function(rec){
+    return rec.firstName === firstName
+  })
+}
+
 let calculatePayroll = function(arrayOfEmployeeRecords){
     return arrayOfEmployeeRecords.reduce(function(memo, rec){
         return memo + allWagesFor.call(rec)
     }, 0)
 }
 
-let findEmployeebyFirstName = function(srcArray, firstName) {
-  return srcArray.find(function(rec){
-    return rec.firstName === firstName
-  })
-}
+
